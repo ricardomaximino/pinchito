18408bb4a9abe337314b487263d51bc3655c118f
Ricardo Maximino <ricardo.maximino@innobadev.altia.es>
2026-06-22 12:29:55 +0200

    First commit

---
+++ src/main/java/es/brasatech/pinchito/PinchitoApplication.java
@@ -0,0 +1,13 @@
+package es.brasatech.pinchito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PinchitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PinchitoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/brasatech/pinchito/PinchitoApplicationTests.java
@@ -0,0 +1,13 @@
+package es.brasatech.pinchito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PinchitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
